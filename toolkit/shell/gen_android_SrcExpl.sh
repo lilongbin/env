1d3f4313cb71883426e2a406ce48a91b8d8f8be6
Longbin Li <longbin.li@aptiv.com>
2020-10-28 17:14:31 +0800

    update gen_kernel_SrcExpl.sh

--- toolkit/shell/gen_android_SrcExpl.sh
+++ toolkit/shell/gen_android_SrcExpl.sh
@@ -4,75 +4,79 @@ clear
 echo "This script used to generate cscope files and tags for android source code"
 FILENAME=cscope.files
 
-function gencscopef()
+function gen_cscope_indexer()
 {
-	echo "Searching *.h *.c *.cpp ... from $PWD"
-	
-	find . -name "out" -prune \
-			 -o -iname "*.[hc]" \
-			 -o -iname "*.[hc]pp" \
-			 > ${FILENAME}
-	#		 | sed -n "s%^\.%$PWD%p" \
-	
-	if [[ $? == 0 ]] ;then
-		[[ -s ${FILENAME} ]] && echo "generate files: " && ls -lh ${FILENAME} 
-	else
-		echo "error occured, ${FILENAME} not be generated" && exit 1
-	fi
-	# sed -n "s/old/new/p"
-	# sed -n "s%$old%$new%p" use % to replace if used var contains of "/"
-	
-	echo "generating cscope index files ..."
-	cscope -bkq -i ${FILENAME}
-	
-	[[ $? == 0  ]] && echo "generated files: " && ls -lh cscope*
+    local manual=1
+
+    if [ "${manual}" = "1" ] ;then
+        echo "Searching *.h *.c *.cc *.hpp *.cpp ... from $PWD"
+        # sed use % to replace if used var contains of "/"
+        find . -name "out" -prune \
+            -o -iname "*.[hc]" \
+            -o -iname "*.[hc]pp" \
+            -o -iname "*.cc" \
+            > ${FILENAME}
+            #| sed -n "s%^\.%$PWD%p" > ${FILENAME}
+        if [ "$?" != "0" ] ;then
+            echo "error occured, ${FILENAME} not be generated"
+            exit 1
+        fi
+        [ -s ${FILENAME} ] && echo "generate files: " && ls -lh ${FILENAME} 
+
+        echo "generating cscope index files ..."
+        cscope -bkq -i ${FILENAME}
+    else
+        #cscope-indexer -r
+        cscope -Rbq
+    fi
+    [ $? == 0 ] && echo "generated files: " && ls -lh cscope*
 }
 
-function gencscopefi()
-{
-	cscope-indexer -r -v
+function create_cscope_files() {
+    if [ -f cscope.out ] ;then
+        read -p "        update cscope files ? <y/n> " select
+        if [ "${select,,}" == "y" ] ;then
+            rm cscope.out
+            gen_cscope_indexer
+        else
+            echo "not updated cscope files"
+        fi
+    else
+        gen_cscope_indexer
+    fi
 }
 
-if [[ -f cscope.out  ]] ;then
-	read -p "		update cscope files ? <y/n> " select
-	if [[ "$select" == "y" ]] ;then
-		rm cscope.out
-		# gencscopef
-		gencscopefi
-	else
-		echo "not updated cscope files"
-	fi
-else
-	# gencscopef
-	gencscopefi
-fi
 
-
-function genctags()
+function gen_ctags_indexer()
 {
-echo "generating ctags file ..."
-ctags -R \
-		  --langmap=c:+.h \
-		  --languages=c,c++
-#		  --links=yes \
-#		  -I __THROW,__wur,__nonnull+  \
-#		  --c++-kinds=+p --c-kinds=+px-n \
-#		  --fields=+ialtfS --extra=+q \
-#		  --file-scope=yes \
-#		  $PWD
-echo "preparations over" && ls -lh tags
-
+    echo "generating tags file ..."
+    #ctags -R *
+    ctags -R \
+        --langmap=c:+.h \
+        --languages=c,c++
+    #          --links=yes \
+    #          -I __THROW,__wur,__nonnull+  \
+    #          --c++-kinds=+p --c-kinds=+px-n \
+    #          --fields=+ialtfS --extra=+q \
+    #          --file-scope=yes \
+    #          $PWD
+    echo "generating tags file complete" && ls -lh tags
 }
 
-if [[ -f tags ]] ;then
-	read -p "		update tags file ? <y/n> " select
-	if [[ "$select" == "y" ]] ;then
-		rm tags
-		genctags
-	else
-		echo "Do not update tags"
-	fi
-else
-	genctags
-fi
+function create_ctags_files() {
+    if [ -f tags ] ;then
+        read -p "		update tags file ? <y/n> " select
+        if [ "${select,,}" == "y" ] ;then
+            rm tags
+            gen_ctags_indexer
+        else
+            echo "not updated tags"
+        fi
+    else
+        gen_ctags_indexer
+    fi
+}
+
+create_cscope_files
+create_ctags_files
 
--- toolkit/shell/gen_kernel_SrcExpl.sh
+++ toolkit/shell/gen_kernel_SrcExpl.sh
@@ -1,65 +1,82 @@
 #! /bin/bash
 # created by Longbin_Li <beangr@163.com>
 clear
-echo "use this script to generate cscope files and tags for linux kernel"
+echo "This script used to generate cscope files and tags for kernel source code"
 FILENAME=cscope.files
 
-function gencscopef()
+function gen_cscope_indexer()
 {
-	echo "Searching *.h *.c ... from $PWD"
-	
-	# find . -iname "*.[hc]" \
-			 # | sed -n "s%^\.%$PWD%p" \
-			 # > ${FILENAME}
-	
-	# if [[ $? == 0 ]] ;then
-		# [[ -s ${FILENAME} ]] && echo "generate files: " && ls -lh ${FILENAME} 
-	# else
-		# echo "error occured, ${FILENAME} not be generated" && exit 1
-	# fi
-	# sed -n "s/old/new/p"
-	# sed -n "s%$old%$new%p" use % to replace if used var contains of "/"
-	
-	echo "generating cscope index files ..."
-	# cscope -bkq -i ${FILENAME}
-	cscope-indexer -r
-	
-	# [[ $? == 0  ]] && echo "generated files: " && ls -lh cscope*
+    local manual=1
 
+    if [ "${manual}" = "1" ] ;then
+        echo "Searching *.h *.c *.cc *.hpp *.cpp ... from $PWD"
+        # sed use % to replace if used var contains of "/"
+        find . -name "out" -prune \
+            -o -iname "*.[hc]" \
+            -o -iname "*.[hc]pp" \
+            -o -iname "*.cc" \
+            | sed -n "s%^\.%$PWD%p" > ${FILENAME}
+            #> ${FILENAME}
+        if [ "$?" != "0" ] ;then
+            echo "error occured, ${FILENAME} not be generated"
+            exit 1
+        fi
+        [ -s ${FILENAME} ] && echo "generate files: " && ls -lh ${FILENAME} 
+
+        echo "generating cscope index files ..."
+        cscope -bkq -i ${FILENAME}
+    else
+        #cscope-indexer -r
+        cscope -Rbq
+    fi
+    [ $? == 0 ] && echo "generated files: " && ls -lh cscope*
 }
 
-if [[ -f cscope.out  ]] ;then
-	read -p "		update cscope files ? <y/n> " select
-	if [[ "$select" == "y" ]] ;then
-		rm cscope.out
-		gencscopef
-	else
-		echo "not updated cscope files"
-	fi
-else
-	gencscopef
-fi
-
-
-function genctags()
-{
-	echo "generating tags file ..."
-	ctags -R *
-	# --langmap=c:+.h \
-			  # --fields=+lS \
-			  # $PWD
-	echo "preparations over" && ls -lh tags
+function create_cscope_files() {
+    if [ -f cscope.out ] ;then
+        read -p "        update cscope files ? <y/n> " select
+        if [ "${select,,}" == "y" ] ;then
+            rm cscope.out
+            gen_cscope_indexer
+        else
+            echo "not updated cscope files"
+        fi
+    else
+        gen_cscope_indexer
+    fi
 }
 
-if [[ -f tags ]] ;then
-	read -p "		update tags file ? <y/n> " select
-	if [[ "$select" == "y" ]] ;then
-		rm tags
-		genctags
-	else
-		echo "not updated tags"
-	fi
-else
-	genctags
-fi
+
+function gen_ctags_indexer()
+{
+    echo "generating tags file ..."
+    ctags -R *
+    #ctags -R \
+    #    --langmap=c:+.h \
+    #    --languages=c,c++
+    #          --links=yes \
+    #          -I __THROW,__wur,__nonnull+  \
+    #          --c++-kinds=+p --c-kinds=+px-n \
+    #          --fields=+ialtfS --extra=+q \
+    #          --file-scope=yes \
+    #          $PWD
+    echo "generating tags file complete" && ls -lh tags
+}
+
+function create_ctags_files() {
+    if [ -f tags ] ;then
+        read -p "		update tags file ? <y/n> " select
+        if [ "${select,,}" == "y" ] ;then
+            rm tags
+            gen_ctags_indexer
+        else
+            echo "not updated tags"
+        fi
+    else
+        gen_ctags_indexer
+    fi
+}
+
+create_cscope_files
+create_ctags_files
 
